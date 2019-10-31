053918cfa4a9146dfc9874513810472e86f563db
Huaxin Yao <yao.huaxin@qq.com>
2019-10-31 10:45:18 +0800

    add PSNR sorting

--- benchmarks/PSNRComputing.py
+++ benchmarks/PSNRComputing.py
@@ -4,7 +4,8 @@ import skimage.io as skimage_io
 from os import listdir
 from os.path import isfile, isdir, join, basename
 
-originalImageDirectory = "../../dataset/images/eligible_files"
+#sourceImageDirectory = "../../dataset/images/eligible_files"
+sourceImageDirectory = "../../dataset/images/tencentResults/guetzli_result"
 processedImageDirectory = "./output.HDD"
 
 def psnr(img1, img2):
@@ -14,54 +15,79 @@ def psnr(img1, img2):
     PIXEL_MAX = 255.0
     return 20 * math.log10(PIXEL_MAX / math.sqrt(mse))
 
-originalImageFiles = []
-for f in listdir(originalImageDirectory):
+sourceImageFiles = {}
+for f in listdir(sourceImageDirectory):
     if f.endswith(".jpg"):
-        fileName = join(originalImageDirectory, f)
-        originalImageFiles.append(fileName)
+        key = f.replace("out_test_","").replace(".jpg","")
+        #print(key)
+        #print(f)
+        fileName = join(sourceImageDirectory, f)
+        sourceImageFiles[key] = fileName
+'''
+for key in sourceImageFiles:
+    print(key, sourceImageFiles[key])
+'''
 
-#for f in originalImageFiles: print(f)
-
-processedImageFiles = []
+processedImageFiles = {}
 for f in listdir(processedImageDirectory):
     if isdir(join(processedImageDirectory, f)):
         subDir = join(processedImageDirectory, f)
         for ff in listdir(subDir):
             if ff.endswith(".jpg"):
+                key = ff.replace(".jpg","")
                 fileName = join(subDir, ff)
-                #print(fileName)
-                processedImageFiles.append(fileName)
-                
-#for f in processedImageFiles: print(f)
+                processedImageFiles[key] = fileName
+'''
+for key in processedImageFiles:
+    print(key, processedImageFiles[key])
+'''
 
 maxPSNR = 0
 minPSNR = 100
+PSNRsOfAllImages = {}
+imagesGivenByTencentButNotInIBMResult = {}
 
-for orignalFile in originalImageFiles:
-    found = False
-    baseNameOfOriginalFile = basename(orignalFile)
-    for processedFile in processedImageFiles:
-        baseFileNameOfProcessed = basename(processedFile)
-        if baseNameOfOriginalFile == baseFileNameOfProcessed:
-            #print(baseNameOfOriginalFile)
-            found = True
-            break
+for orignalFileKey in sourceImageFiles:
+    if orignalFileKey in processedImageFiles:
+        sourceFile = sourceImageFiles[orignalFileKey]
+        processedFile = processedImageFiles[orignalFileKey]
         
-    if found:
-        print("Debug:", orignalFile, processedFile)
-        
-        original = skimage_io.imread(orignalFile)
+        print("Debug: Found file, ID: {}, source file: {}, processed file: {}.".format(
+                orignalFileKey, sourceFile, processedFile))
+
+        original = skimage_io.imread(sourceFile)
         processed = skimage_io.imread(processedFile)
         
         if original.ndim != processed.ndim:
-            print("Mismatch:", basename(orignalFile), "ndim is not equal, will not calculated:", original.shape, processed.shape)
+            print("Dimension mismatch:", basename(sourceFile), "ndim is not equal, will not calculated:", original.shape, processed.shape)
         else:
             d=psnr(original, processed)
+            if d > maxPSNR: maxPSNR = d
+            if d < minPSNR: minPSNR = d
+            
+            PSNRsOfAllImages[orignalFileKey] = d
+            
             if d == 100:
-                print("Saturate:", basename(orignalFile), "PSNR is 100.")
+                print("Image identify:", basename(sourceFile), "PSNR is 100.")
             else:
-                print("Normal:", basename(orignalFile), "PSNR:", d)
-                if d > maxPSNR: maxPSNR = d
-                if d < minPSNR: minPSNR = d
-        
-print(maxPSNR, minPSNR)
+                print("Image not identify:", basename(sourceFile), "PSNR:", d)
+
+    else:
+        print("Debug: Not found file, ID: {}, source file: {}.".format(orignalFileKey, sourceImageFiles[orignalFileKey]))
+        imagesGivenByTencentButNotInIBMResult[orignalFileKey] = sourceImageFiles[orignalFileKey]
+        pass
+
+print("==== ==== ==== ==== summary ==== ==== ==== ====")
+print("Images given by Tencent, but not in IBM result:")
+for item in imagesGivenByTencentButNotInIBMResult.items():
+    print("    ", item)
+
+PSNRsOfAllImagesSortedByValue = sorted(PSNRsOfAllImages.items(), key=lambda x: x[1])
+print("Total number of images compared", len(PSNRsOfAllImages.items()))
+#print("Total picture compared", len(PSNRsOfAllImagesSortedByValue))
+
+print("PSNR range: [", maxPSNR, minPSNR, "]")
+
+for index in range(0, len(PSNRsOfAllImagesSortedByValue)):
+    print(PSNRsOfAllImagesSortedByValue[index])
+
